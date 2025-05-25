4e2b6cf06a5c82deeadf42a9ac13918428ccd520
GabiMarques1002 <gabriella.marques.silva@escola.pr.gov.br>
2025-05-25 14:46:11 -0300

    Add files via upload

---
+++ conversoes.js
@@ -0,0 +1,17 @@
+// tipo de dado
+// booleanos
+
+//conversão implícida
+
+const numero = 456;
+const numeroString = Number("456a");
+
+// Number()
+// String()
+console.log( numero + numeroString)
+
+
+
+//conversão explícita
+
+
---
+++ truthy-falsy.js
@@ -0,0 +1,33 @@
+//boolean
+
+const usuarioLogado = true
+const contapaga = false
+
+//truthy ou falsy
+
+// 0 = false
+// 1 = true
+
+console.log(0 == false)
+console.log('' == false)
+console.log(1 == true)
+
+
+// undefiled
+
+// null: vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+let numero = 3;
+let texto = "alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
